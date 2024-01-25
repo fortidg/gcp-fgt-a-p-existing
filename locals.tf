399beab34bd3c90728232492851a1cf98d4162e0
fortidg <dgooch@fortinet.com>
2024-01-25 18:14:24 +0000

    fixed license call for fgt-2

--- locals.tf
+++ locals.tf
@@ -247,7 +247,7 @@ locals {
 
       metadata = {
         user-data = data.template_file.template_file["fgt2-template"].rendered
-        license   = local.fortigate_license_files["fgt2_instance"].name != null ? file(local.fortigate_license_files["branch_fgt_instance"].name) : null
+        license   = local.fortigate_license_files["fgt2_instance"].name != null ? file(local.fortigate_license_files["fgt2_instance"].name) : null
       }
       service_account_scopes    = ["cloud-platform"]
       allow_stopping_for_update = true
